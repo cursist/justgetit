e1d692325ea4331804411bd6d6ff193298523d76
hansdewitte <hans.dewitte@vdabcampus.be>
2019-05-23 09:55:48 +0200

    eerste commit

---
+++ src/test/java/be/vdab/justgetit/JustgetitApplicationTests.java
@@ -0,0 +1,16 @@
+package be.vdab.justgetit;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JustgetitApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
