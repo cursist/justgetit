243211fd59ca4e1452611f5de5c8ddc6042b567f
Sam Meulen <sam.meulen@vdabcampus.be>
2019-05-24 10:50:05 +0200

    Merge branch 'develop' of https://github.com/cursist/justgetit into develop
    # Conflicts:
    #	src/main/java/be/vdab/justgetit/repositories/CategorieRepository.java
    #	src/main/java/be/vdab/justgetit/services/CategorieService.java
    #	src/main/java/be/vdab/justgetit/services/DefaultCategorieService.java
    #	src/main/resources/application.properties
    #	src/test/java/be/vdab/justgetit/repositories/DataSourceTest.java

---
+++ src/main/java/be/vdab/justgetit/services/DefaultSubcategorieService.java
@@ -0,0 +1,60 @@
+package be.vdab.justgetit.services;
+
+import be.vdab.justgetit.entities.Subcategorie;
+import be.vdab.justgetit.repositories.SubcategorieRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true, isolation = Isolation.READ_COMMITTED)
+public class DefaultSubcategorieService implements SubcategorieService {
+    private SubcategorieRepository subcategorieRepository;
+
+    public DefaultSubcategorieService(SubcategorieRepository subcategorieRepository) {
+        this.subcategorieRepository = subcategorieRepository;
+    }
+
+    @Override
+    public List<Subcategorie> findAll(){
+        return subcategorieRepository.findAll();
+    }
+
+    @Override
+    public Optional<Subcategorie> findById(long id){
+        return subcategorieRepository.findById(id);
+    }
+
+    @Override
+    public List<Subcategorie> findByCategorieId(long id) {
+        return subcategorieRepository.findByCategorieCategorieId(id);
+    }
+
+    @Override
+    public Subcategorie findByNaam(String str) {
+        return subcategorieRepository.findByNaam(str);
+    }
+
+    @Override
+    public List<Subcategorie> findByNameLike(String str) {
+        return subcategorieRepository.findByNaamLike(str);
+    }
+
+    @Override
+    public List<Subcategorie> findByEigenschappenNaamLike(String str) {
+        return subcategorieRepository.findByEigenschappenNaamLike(str);
+    }
+
+    @Override
+    public void Save(Subcategorie subcategorie) {
+        subcategorieRepository.save(subcategorie);
+    }
+
+    @Override
+    public void Delete(Subcategorie subcategorie) {
+        subcategorieRepository.delete(subcategorie);
+    }
+}
---
+++ src/test/java/be/vdab/justgetit/entities/ProductTest.java
@@ -0,0 +1,39 @@
+package be.vdab.justgetit.entities;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertEquals;
+
+public class ProductTest {
+
+    private Merk merk;
+    private Categorie categorie;
+    private Subcategorie subcategorie;
+    private Product product;
+
+    @Before
+    public void before(){
+        merk = new Merk(1l,"test", BigDecimal.TEN,
+                BigDecimal.TEN);
+        categorie = new Categorie(1l,"test");
+        subcategorie = new Subcategorie(1l,"test",BigDecimal.TEN,
+                BigDecimal.TEN, categorie);
+
+        product = new Product(1l,"test",BigDecimal.TEN,
+                 BigDecimal.TEN,BigDecimal.TEN,1,0,
+                merk,subcategorie);
+    }
+
+    @Test
+    public void naamVanMerkisCorrectVanProduct(){
+        assertEquals("test", product.getMerk().getNaam());
+    }
+
+    @Test
+    public void naamVanSubcategorieCorrectVanProduct(){
+        assertEquals("test",product.getSubcategorie().getNaam());
+    }
+}
