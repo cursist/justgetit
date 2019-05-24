c3e4b021d53c692a45fd797937a6177fee319168
Sam Meulen <sam.meulen@vdabcampus.be>
2019-05-24 09:26:26 +0200

    categorieservice, defaultcategorieservice, test alles ok

---
+++ src/main/java/be/vdab/justgetit/services/DefaultCategorieService.java
@@ -0,0 +1,30 @@
+package be.vdab.justgetit.services;
+
+import be.vdab.justgetit.domain.Categorie;
+import be.vdab.justgetit.repositories.CategorieRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional(readOnly = true , isolation = Isolation.READ_COMMITTED)
+public class DefaultCategorieService implements CategorieService {
+
+    private final CategorieRepository categorieRepository;
+
+    public DefaultCategorieService(CategorieRepository categorieRepository) {
+        this.categorieRepository = categorieRepository;
+    }
+
+    @Override
+    public Optional<Categorie> findById(long id) {
+       return categorieRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Categorie> findByNaam(String naam) {
+        return categorieRepository.findByNaam(naam);
+    }
+}
