f2b3d660eb8bcf3da0a4018a333f0c8d46a9cc3e
Sam Meulen <sam.meulen@vdabcampus.be>
2019-05-24 10:53:00 +0200

    categorieservice + tests

--- src/main/java/be/vdab/justgetit/services/DefaultCategorieService.java
+++ src/main/java/be/vdab/justgetit/services/DefaultCategorieService.java
@@ -1,5 +1,6 @@
 package be.vdab.justgetit.services;
 
+<<<<<<< HEAD
 import be.vdab.justgetit.domain.Categorie;
 import be.vdab.justgetit.repositories.CategorieRepository;
 import org.springframework.stereotype.Service;
@@ -12,6 +13,14 @@ import java.util.Optional;
 @Transactional(readOnly = true , isolation = Isolation.READ_COMMITTED)
 public class DefaultCategorieService implements CategorieService {
 
+=======
+import be.vdab.justgetit.entities.Categorie;
+import be.vdab.justgetit.repositories.CategorieRepository;
+
+import java.util.List;
+
+public class DefaultCategorieService implements CategorieService {
+>>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
     private final CategorieRepository categorieRepository;
 
     public DefaultCategorieService(CategorieRepository categorieRepository) {
@@ -19,6 +28,7 @@ public class DefaultCategorieService implements CategorieService {
     }
 
     @Override
+<<<<<<< HEAD
     public Optional<Categorie> findById(long id) {
        return categorieRepository.findById(id);
     }
@@ -26,5 +36,9 @@ public class DefaultCategorieService implements CategorieService {
     @Override
     public Optional<Categorie> findByNaam(String naam) {
         return categorieRepository.findByNaam(naam);
+=======
+    public List<Categorie> findByNaamLike(String naam){
+        return categorieRepository.findByNaamLike(naam);
+>>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
     }
 }
--- src/test/java/be/vdab/justgetit/repositories/DataSourceTest.java
+++ src/test/java/be/vdab/justgetit/repositories/DataSourceTest.java
@@ -4,7 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+<<<<<<< HEAD
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+=======
+import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
+>>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
@@ -12,15 +16,26 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
+<<<<<<< HEAD
 @DataJpaTest
+=======
+@JdbcTest
+>>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DataSourceTest {
     @Autowired
     private DataSource dataSource;
+<<<<<<< HEAD
 
     @Test
     public void getConnection() throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
+=======
+    @Test
+    public void getConnection()throws SQLException {
+        try(Connection connection = dataSource.getConnection()){
+
+>>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
         }
     }
 }
