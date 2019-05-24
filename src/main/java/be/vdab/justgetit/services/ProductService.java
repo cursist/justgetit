d3961f93184a9d3fff32c6ed53901bae46d197b2
geert moris <geert.moris@vdabcampus.be>
2019-05-24 13:36:14 +0200

    alles in juiste package en imports ok

--- src/main/java/be/vdab/justgetit/services/ProductService.java
+++ src/main/java/be/vdab/justgetit/services/ProductService.java
@@ -1,11 +1,22 @@
 package be.vdab.justgetit.services;
 
+import be.vdab.justgetit.entities.Merk;
 import be.vdab.justgetit.entities.Product;
+import be.vdab.justgetit.entities.Subcategorie;
 
+import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
 public interface ProductService {
     List<Product> findAll();
-    Optional<Product> findById(long id);
+    Optional<Product> findById(long productId);
+    List<Merk> findByMerkId(long merkId);
+    List<Subcategorie> findBySubcategorieId(long subcategorieId);
+    List<Product> findByVerkoopprijsBetween(BigDecimal van , BigDecimal tot);
+    List<Product> findByBesteldIsNull();
+    List<Product> findByVoorraadKleinerDan(BigDecimal aantal);
+    Product save(Product product);
+    void delete(Product product);
+    List<Product>findByNaamContaining(String zoekString);
 }
