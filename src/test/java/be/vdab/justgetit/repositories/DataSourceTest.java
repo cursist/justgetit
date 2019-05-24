1fa3e0ff5b579d4a14ecabf629794599967b6302
Sam Meulen <sam.meulen@vdabcampus.be>
2019-05-24 11:28:33 +0200

    datasourcetest

--- src/test/java/be/vdab/justgetit/repositories/DataSourceTest.java
+++ src/test/java/be/vdab/justgetit/repositories/DataSourceTest.java
@@ -4,11 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-<<<<<<< HEAD
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-=======
-import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
->>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.sql.DataSource;
@@ -16,26 +12,15 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 @RunWith(SpringRunner.class)
-<<<<<<< HEAD
 @DataJpaTest
-=======
-@JdbcTest
->>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class DataSourceTest {
     @Autowired
     private DataSource dataSource;
-<<<<<<< HEAD
 
     @Test
     public void getConnection() throws SQLException {
         try (Connection connection = dataSource.getConnection()) {
-=======
-    @Test
-    public void getConnection()throws SQLException {
-        try(Connection connection = dataSource.getConnection()){
-
->>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
         }
     }
 }
