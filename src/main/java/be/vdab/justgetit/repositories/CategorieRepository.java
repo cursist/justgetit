01ade9562d8e64fb48e48448699eb4454b12790b
Sam Meulen <sam.meulen@vdabcampus.be>
2019-05-24 11:19:22 +0200

    categorierepository

--- src/main/java/be/vdab/justgetit/repositories/CategorieRepository.java
+++ src/main/java/be/vdab/justgetit/repositories/CategorieRepository.java
@@ -1,6 +1,6 @@
 package be.vdab.justgetit.repositories;
 
-<<<<<<< HEAD
+
 import be.vdab.justgetit.domain.Categorie;
 
 import java.util.Optional;
@@ -8,14 +8,4 @@ import java.util.Optional;
 public interface CategorieRepository {
     Optional<Categorie> findById(long id);
     Optional<Categorie> findByNaam(String naam);
-
-=======
-import be.vdab.justgetit.entities.Categorie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
-public interface CategorieRepository extends JpaRepository <CategorieRepository, Long>{
-    List<Categorie> findByNaamLike(String naam);
->>>>>>> d79fca2b90cfb48321ae86c7c45a3acd3b69bf32
 }
