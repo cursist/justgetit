baaf032b3bfdb1c932ade65b726ceca74723a7bf
hansdewitte <hans.dewitte@vdabcampus.be>
2019-05-28 15:03:28 +0200

    aanpassing testSQL

--- src/test/resources/insertSubcategorie.sql
+++ src/test/resources/insertSubcategorie.sql
@@ -1,3 +1,2 @@
-insert into subcategorieen (naam,categorieId) values('TestSubcategorie', (select categorieId from categorieen where naam = 'testD'));
-insert into subcategorieen (naam,categorieId) values('Test2Subcategorie', (select categorieId from categorieen where naam = 'testD'));
-insert into subcategorieen (naam,categorieId) values('testSC', (select categorieId from categorieen where naam = 'testD'));
+insert into subcategorieen (naam,categorieId) values('TestSubcategorie', (select categorieId from categorieen where naam = 'testC'));
+insert into subcategorieen (naam,categorieId) values('Test2Subcategorie', (select categorieId from categorieen where naam = 'testC'));
