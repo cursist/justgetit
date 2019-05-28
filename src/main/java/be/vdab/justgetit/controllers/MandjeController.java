6ede23d237908b54a1148e8ca423057d02a20afd
cAzare <christof.azare@gmail.com>
2019-05-28 13:07:52 +0200

    met aantalForm en MandjeController

--- src/main/java/be/vdab/justgetit/controllers/MandjeController.java
+++ src/main/java/be/vdab/justgetit/controllers/MandjeController.java
@@ -1,10 +1,17 @@
 package be.vdab.justgetit.controllers;
 
+import be.vdab.justgetit.forms.AantalForm;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("mandje")
 public class MandjeController {
 
+    @GetMapping
+    ModelAndView toonMandje () {
+        return new ModelAndView("mandje", "mandje", new AantalForm(null));
+    }
 }
