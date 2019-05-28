d9cee6c6aa4c50f1435e1563a05c960f4e725fb7
cAzare <christof.azare@gmail.com>
2019-05-28 11:33:25 +0200

    added beginning of mandje class, html and controller

---
+++ src/main/java/be/vdab/justgetit/controllers/MandjeController.java
@@ -0,0 +1,10 @@
+package be.vdab.justgetit.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("mandje")
+public class MandjeController {
+
+}
