18a3accb24204768812eebf5cb936292417e73b4
hansdewitte <hans.dewitte@vdabcampus.be>
2019-05-28 15:21:52 +0200

    kleine refactor

--- src/main/java/be/vdab/justgetit/controllers/BediendeController.java
+++ src/main/java/be/vdab/justgetit/controllers/BediendeController.java
@@ -1,4 +1,4 @@
-package be.vdab.justgetit.bediende.controller;
+package be.vdab.justgetit.controllers;
 
 import be.vdab.justgetit.entities.Product;
 import be.vdab.justgetit.services.BediendeService;
@@ -14,20 +14,16 @@ import java.util.List;
 @Controller
 @RequestMapping("bediende")
 public class BediendeController {
-
-    private final BediendeService bediendeService;
     private final ProductService productService;
     private final SubcategorieService subcatService;
 
-    public BediendeController(BediendeService bediendeService, ProductService productService, SubcategorieService subcatService) {
-        this.bediendeService = bediendeService;
+    public BediendeController(ProductService productService, SubcategorieService subcatService) {
         this.productService = productService;
         this.subcatService = subcatService;
     }
 
     @GetMapping
     ModelAndView bediendeModelAndView() {
-/*        List <Product> product= productService.findAll();*/
         ModelAndView modelAndView = new ModelAndView("bediende");
         modelAndView.addObject("productenlijst", productService.findAll());
         modelAndView.addObject("subcatlijst", subcatService.findAll());
