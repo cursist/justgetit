22aebd53d3fcc563c0554450eb251770b239c768
Thierry <verheyen.thierry@gmail.com>
2019-06-05 11:30:50 +0200

    zoeken op naam werkt

--- src/main/java/be/vdab/justgetit/controllers/ProductController.java
+++ src/main/java/be/vdab/justgetit/controllers/ProductController.java
@@ -1,11 +1,17 @@
 package be.vdab.justgetit.controllers;
 
+import be.vdab.justgetit.entities.Product;
 import be.vdab.justgetit.services.ProductService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 
@@ -14,6 +20,8 @@ public class ProductController {
 
 
     private ProductService productService;
+
+
 
     public ProductController( ProductService productService) {
         this.productService = productService;
@@ -29,3 +37,5 @@ public class ProductController {
     }
 
 }
+
+
